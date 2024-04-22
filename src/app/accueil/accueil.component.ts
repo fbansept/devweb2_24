36048322910adb9366a0bf766b5e0539d967dbca
fbansept <bansept.franck@gmail.com>
2024-04-22 10:48:02 +0200

    affichage article

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,22 +1,24 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, MatCardModule, MatButtonModule],
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.scss'
+  styleUrl: './accueil.component.scss',
 })
 export class AccueilComponent {
+  http: HttpClient = inject(HttpClient);
 
-  http: HttpClient = inject(HttpClient)
-  
+  listeArticles: any = [];
+
   ngOnInit() {
     this.http
       .get('http://localhost/backend-angular/articles.php')
-      .subscribe((resultat) => console.log(resultat));
-
+      .subscribe((resultat) => (this.listeArticles = resultat));
   }
-
 }
